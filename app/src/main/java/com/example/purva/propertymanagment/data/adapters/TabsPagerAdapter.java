b06a85e7a126e9c73077fede80b99f0111eb950b
purva <purva.vansia@yahoo.com>
2018-04-25 16:39:20 -0500

    Added Landlord email EditText for Tenant sign up

--- app/src/main/java/com/example/purva/propertymanagment/data/adapters/TabsPagerAdapter.java
+++ app/src/main/java/com/example/purva/propertymanagment/data/adapters/TabsPagerAdapter.java
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import com.example.purva.propertymanagment.ui.signup.TenantSignupFragment;
+import com.example.purva.propertymanagment.ui.signup.tenant.TenantSignupFragment;
 import com.example.purva.propertymanagment.ui.signup.landlord.LandlordSignup;
 
 /**
