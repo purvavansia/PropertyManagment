0c7dd11e99bb2819546a3b3d2ba2736530b2b6f1
Rui <liurui320916@gmail.com>
2018-04-26 15:06:16 -0500

    nothing commited

--- app/src/main/java/com/example/purva/propertymanagment/ui/home/MainActivity.java
+++ app/src/main/java/com/example/purva/propertymanagment/ui/home/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.purva.propertymanagment.ui.home;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.purva.propertymanagment.R;
-
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
